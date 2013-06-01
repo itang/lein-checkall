62c53c44b9c27d3ee1b58ef24b17f711ead285e6
itang <live.tang@gmail.com>
2013-06-01 16:02:39 +0800

    0.1 release && deploy to clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-checkall "0.1.0-SNAPSHOT"
+(defproject lein-checkall "0.1"
   :description "lein check && lein kibit && lein eastwood && lein bikeshed"
   :url "http://lein-checkall.itang.me"
   :license {:name "Eclipse Public License"
@@ -6,5 +6,10 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [lein-kibit "0.0.8"]
                  [jonase/eastwood "0.0.2"]
-                 [lein-bikeshed "0.1.0"]]
-  :eval-in-leiningen true)
+                 [lein-bikeshed "0.1.3"]]
+  :eval-in-leiningen true
+  :pom-addition [:developers [:developer
+                              [:id "itang"]
+                              [:name "唐古拉山"]
+                              [:url "http://www.itang.me"]
+                              [:email "live.tang@gmail.com"]]])
